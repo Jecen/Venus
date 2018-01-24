3d78b55e0cf3433b6941a7745f0c5c476b917157
sjc <shijiachen@hztianque.com>
2018-01-24 21:17:00 +0800

    add avatar png

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <header>
-      <span>Vue.js PWA</span>
+      <span>ousututu</span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
+      <img src="./assets/avatar.png" alt="ousututu">
       <router-view></router-view>
     </main>
   </div>
@@ -22,7 +22,7 @@ body {
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
@@ -32,12 +32,16 @@ main {
   text-align: center;
   margin-top: 40px;
 }
+main img {
+  width: 200px;
+  height: 200px;
+}
 
 header {
   margin: 0;
   height: 56px;
   padding: 0 16px 0 24px;
-  background-color: #35495E;
+  background-color: #35495e;
   color: #ffffff;
 }
 
@@ -46,7 +50,7 @@ header span {
   position: relative;
   font-size: 20px;
   line-height: 1;
-  letter-spacing: .02em;
+  letter-spacing: 0.02em;
   font-weight: 400;
   box-sizing: border-box;
   padding-top: 16px;
